741575b70323dba04337bd9ca295df2106152a4c
Pribytov <igor-pribytov@yandex.ru>
2022-08-06 14:41:44 +0300

    Тень календаря

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,14 @@ import moment  from "moment";
 import { Header } from "../Header";
 import { Monitor } from "../Monitor";
 import { CalendarGrid } from "../CalendarGrid";
+import styled from "styled-components";
+
+const ShadowWrapper = styled.div`
+  border: 1px solid #464648;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 0 0 1px #1a1a1a, 0 8px 20px 6px #888;
+`;
 
 function App() {
  
@@ -12,12 +20,12 @@ function App() {
   const startDay = moment().startOf('month').startOf('week');
   
   return (
-    <div className="App">
+    <ShadowWrapper>
       <Header />
       <Monitor />
       <CalendarGrid startDay={startDay}/>
 
-    </div>
+    </ShadowWrapper>
   );
 }
 
